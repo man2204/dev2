616055d244638a1b624e8ce6f4d019d053030b35
Manish <manishkumar.41@gmail.com>
2021-07-13 23:29:52 +0530

    changes

--- hello.java
+++ hello.java
@@ -1,9 +1,8 @@
 
 # include<iostream.h>
 # include<conio.h>  #dev2
-<<<<<<< HEAD
         print("jai ho)
         print("shree ram")
 }                          
  } 
->>>>>>> 4a8d6323e27d829cf8a8259893d39b361ef95522
+
